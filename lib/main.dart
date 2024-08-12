1ec1c34b438c6a9415bacb295991ac569a6715d7
fayizdev <fayizkottakkaran@gmail.com>
2024-08-12 10:44:59 +0530

    fghjkl,

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List imageView = [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/301',
+      'https://picsum.photos/200/304',
+      'https://picsum.photos/200/305',
+      'https://picsum.photos/200/306',
+      'https://picsum.photos/200/307',
+      'https://picsum.photos/200/308',
+      'https://picsum.photos/200/302',
+      'https://picsum.photos/200/302',
+      'https://picsum.photos/200/302',
+      'https://picsum.photos/200/302',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Column(
+          child: PageView(
             children: [
-              Container(
-                color: Colors.red,
-                child: Text("my title"),
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 60,
+                      color: Colors.red,
+                      child: Text("my title"),
+                    ),
+                    ListView.separated(
+                      shrinkWrap: true,
+                      physics: AlwaysScrollableScrollPhysics(),
+                      itemBuilder: (context, index) => Container(
+                        height: 200,
+                        color: Colors.blue,
+                      ),
+                      separatorBuilder: (context, index) => SizedBox(
+                        height: 20,
+                      ),
+                      itemCount: 10,
+                    ),
+                  ],
+                ),
               ),
-              ListView.separated(
+              Container(
+                color: Colors.yellow,
+              ),
+              PageView.builder(
                 itemBuilder: (context, index) => Container(
-                  height: 200,
-                  color: Colors.blue,
+                  //width: double.infinity,
+                  child: Image.network(imageView[index]),
                 ),
-                separatorBuilder: (context, index) => SizedBox(
-                  height: 20,
-                ),
-                itemCount: 10,
+                itemCount: imageView.length,
               )
             ],
           ),
